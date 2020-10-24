aae57474b81688f4c6fafe72f9628c3e9f8e133a
Priyank Agarwal <priyank-ayron@gmail.com>
2020-10-24 23:07:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const weatherApi = {
+  key: "bdc3cee5ec5fe024aea027166ec6dbd1",
+  baseUrl: "https://api.openweathermap.org/data/2.5/weather",
+};
+
+const searchInputBox = document.getElementById("input-box");
+searchInputBox.addEventListener("keypress", (event) => {
+  if (event.keyCode == 13) {
+    console.log(searchInputBox.value);
+    getWeatherReport(searchInputBox.value);
+    document.querySelector(".weather-body").style.display = "block";
+  }
+});
+
+function getWeatherReport(city) {
+  fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(showWeatherReport);
+}
+
+function showWeatherReport(weather) {
+  console.log(weather);
+  let city = document.getElementById("city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let temp = document.getElementById("temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+  let minmax = document.getElementById("min-max");
+  minmax.innerHTML = `${Math.floor(
+    weather.main.temp_min
+  )}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max)`;
+
+  let weatherType = document.getElementById("weather");
+  weatherType.innerText = `${weather.weather[0].main}`;
+
+  let weatherIconCode = `${weather.weather[0].icon}`;
+  let weatherIcon = document.getElementById("image");
+  weatherIcon.src = `http://openweathermap.org/img/wn/${weatherIconCode}@2x.png`;
+
+  let date = document.getElementById("date");
+  let todayDate = new Date();
+  date.innerText = dateManage(todayDate);
+
+  if (weatherType.textContent == "Clear") {
+    document.body.style.backgroundImage = "url('images/clear.jpg')";
+  } else if (weatherType.textContent == "Clouds") {
+    document.body.style.backgroundImage = "url('images/cloud.jpg')";
+  } else if (weatherType.textContent == "Rain") {
+    document.body.style.backgroundImage = "url('images/rain.jpg')";
+  } else if (weatherType.textContent == "Snow") {
+    document.body.style.backgroundImage = "url('images/snow.jpg')";
+  } else if (weatherType.textContent == "Thunderstorm") {
+    document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
+  } else if (weatherType.textContent == "Haze") {
+    document.body.style.backgroundImage = "url('images/haze.jpg')";
+  }
+}
+
+function dateManage(dateArg) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let year = dateArg.getFullYear();
+  let month = months[dateArg.getMonth()];
+  let date = dateArg.getDate();
+  let day = days[dateArg.getDay()];
+
+  return `${date} ${month} (${day}), ${year}`;
+}
